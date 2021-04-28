2bfdbde01caed0046670d5fdba4077e4bf343b89
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-04-28 02:25:35 -0700

    Added username and password validation and security checks

--- models/User.js
+++ models/User.js
@@ -1,7 +1,11 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-
-class User extends Model { }
+const bcrypt = require('bcrypt');
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 User.init(
     {
@@ -31,6 +35,17 @@ User.init(
         }
     },
     {
+        hooks:
+        {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(userData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
@@ -38,3 +53,5 @@ User.init(
         modelName: 'user'
     }
 );
+
+module.exports = User;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const User = require('../../models');
+const User = require('../../models/User');
 
 //GET /api/users
 router.get('/', (req, res) => {
@@ -46,9 +46,31 @@ router.post('/', (req, res) => {
         });
 });
 
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address.' });
+            return;
+        }
+        // res.json({ user: dbUserData });
+        //Verify data
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect Password' });
+            return;
+        }
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+});
+
 //PUT /api/users/:id
 router.put('/:id', (req, res) => {
     User.update(req.body, {
+        individualHooks: true,
         where: {
             id: req.params.id
         }
