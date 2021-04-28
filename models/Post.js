8350dfbc2f6bd04fb082719d1205622b9535af76
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-04-28 02:59:04 -0700

    added Post model

---
+++ models/Post.js
@@ -0,0 +1,44 @@
+const { dasherize } = require('inflection');
+const { Model, Datatypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+//create the Post model
+class Post extends Model { }
+
+
+Post.init(
+    {
+        id: {
+            type: Datatypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: Datatypes.STRING,
+            allowNull: false
+        },
+        post_url: {
+            type: Datatypes.STRING,
+            allowNull: false,
+            validate: {
+                isUrl: true
+            }
+        },
+        user_id: {
+            type: Datatypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Post;
