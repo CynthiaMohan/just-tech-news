c3020bf287a7a3cd94ac3595c5d7c745e9af7677
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-05-02 12:06:52 -0700

    updates to post/upvote

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -75,32 +75,41 @@ router.post('/', (req, res) => {
             res.status(500).json(err);
         });
 });
+// router.put('/upvote', (req, res) => {
+// Vote.create({
+//     user_id: req.body.user_id,
+//     post_id: req.body.post_id
+// })
+//     .then(() => {
+//         // then find the post we just voted on
+//         return Post.findOne({
+//             where: {
+//                 id: req.body.post_id
+//             },
+//             attributes: [
+//                 'id',
+//                 'post_url',
+//                 'title',
+//                 'created_at',
+//                 // use raw MySQL aggregate function query to get a count of how many votes the post has and return it under the name `vote_count`
+//                 [
+//                     sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+//                     'vote_count'
+//                 ]
+//             ]
+//         })
+//     })
+//     Post.upvote(req.body, { Vote })
+//         .then(updateddbPostData => res.json(updateddbPostData))
+//         .catch(err => {
+//             console.log(err);
+//             res.status(400).json(err);
+//         });
+// });
 router.put('/upvote', (req, res) => {
-    // Vote.create({
-    //     user_id: req.body.user_id,
-    //     post_id: req.body.post_id
-    // })
-    //     .then(() => {
-    //         // then find the post we just voted on
-    //         return Post.findOne({
-    //             where: {
-    //                 id: req.body.post_id
-    //             },
-    //             attributes: [
-    //                 'id',
-    //                 'post_url',
-    //                 'title',
-    //                 'created_at',
-    //                 // use raw MySQL aggregate function query to get a count of how many votes the post has and return it under the name `vote_count`
-    //                 [
-    //                     sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-    //                     'vote_count'
-    //                 ]
-    //             ]
-    //         })
-    //     })
+    // custom static method created in models/Post.js
     Post.upvote(req.body, { Vote })
-        .then(updateddbPostData => res.json(updateddbPostData))
+        .then(updatedPostData => res.json(updatedPostData))
         .catch(err => {
             console.log(err);
             res.status(400).json(err);
