5f7b16c6c56a1645a7bdd6925236c0606931a52c
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-05-02 12:04:58 -0700

    Added upvote feature

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const User = require('../../models/User');
+const { User, Post, Vote } = require('../../models/User');
 
 //GET /api/users
 router.get('/', (req, res) => {
@@ -17,7 +17,19 @@ router.get('/:id', (req, res) => {
         attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
-        }
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Post,
+                attributes: ['title'],
+                through: Vote,
+                as: 'voted_posts'
+            }
+        ]
     })
         .then(dbUserData => {
             if (!dbUserData) {
