ddb274dd39a89c846b85f7b25402172bc600d04e
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-05-04 11:53:17 -0700

    Updated login in users

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -67,22 +67,24 @@ router.post('/', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
     User.findOne({
         where: {
             email: req.body.email
         }
     }).then(dbUserData => {
         if (!dbUserData) {
-            res.status(400).json({ message: 'No user with that email address.' });
+            res.status(400).json({ message: 'No user with that email address!' });
             return;
         }
-        // res.json({ user: dbUserData });
-        //Verify data
+
         const validPassword = dbUserData.checkPassword(req.body.password);
+
         if (!validPassword) {
-            res.status(400).json({ message: 'Incorrect Password' });
+            res.status(400).json({ message: 'Incorrect password!' });
             return;
         }
+
         res.json({ user: dbUserData, message: 'You are now logged in!' });
     });
 });
