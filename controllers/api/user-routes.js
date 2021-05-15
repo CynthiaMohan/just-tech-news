ab20878fbad7ebaab7235245e2508049bf2823d7
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-05-14 23:16:57 -0700

    logout functionality added

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -97,7 +97,17 @@ router.post('/login', (req, res) => {
         });
     });
 });
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(404).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
 
+});
 //PUT /api/users/:id
 router.put('/:id', (req, res) => {
     User.update(req.body, {
